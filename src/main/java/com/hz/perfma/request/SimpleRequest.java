85ab216dbb749823eb25c5e88985a5ab084644a8
xieqiyong <1158902151@qq.com>
2021-08-24 10:14:19 +0800

    first commit

---
+++ src/main/java/com/hz/perfma/request/SimpleRequest.java
@@ -0,0 +1,18 @@
+package com.hz.perfma.request;
+
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+/**
+ * @Description 处理回放请求
+ * @Author liusu <xieqiyong66@gmail.com>
+ * @Version v1.0.0
+ * @Date 2021/8/23
+ */
+public class SimpleRequest extends AbstractJavaSamplerClient {
+
+    public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+        return null;
+    }
+}
